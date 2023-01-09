b7ec1b8de3593310c7b402b7407555ecfe4194cf
harsh6975 <harshu1234ashv@gmail.com>
2023-01-09 17:29:19 +0530

    View Text Style

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! heheh boi </Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>
+          Starting App Development with React Native{" "}
+        </Text>
+      </View>
+      <View>
+        <Text style={styles.text}>
+          React Native do not support div or normal HTML elements.
+        </Text>
+        <Text style={styles.text}>
+          React Native do not normal text. To write text wrap in Text element.
+        </Text>
+        <Text style={styles.text}>
+          React Native do not support inheritence except for the Text element.
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "yellow",
+    padding: 20,
+    marginBottom: 10,
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  text: {
+    padding: 5,
+    backgroundColor: "pink",
+    fontSize: 13,
   },
 });
